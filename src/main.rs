33aad5eac1e06231b7312bcd7a739797263fd51d
hatoo <hato2000@gmail.com>
2019-11-26 22:12:52 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![cfg_attr(feature = "test_generator", feature(generators, generator_trait))]
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(feature = "test_generator")]
+#[allow(dead_code)]
+fn test_generator() {
+    // Use some `yield`.
+    use std::ops::{GeneratorState, Generator};
+    use std::pin::Pin;
+
+    let mut generator = || {
+        yield 1;
+        return "foo"
+    };
+
+    match Pin::new(&mut generator).resume() {
+        GeneratorState::Yielded(1) => {}
+        _ => panic!("unexpected return from resume"),
+    }
+    match Pin::new(&mut generator).resume() {
+        GeneratorState::Complete("foo") => {}
+        _ => panic!("unexpected return from resume"),
+    }
+}
